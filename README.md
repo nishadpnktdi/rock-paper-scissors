1faa5832ce4c3e8923956daf63a003dafb712e96
Nishad PN <nishad.pnktdi@gmail.com>
2018-02-04 23:09:01 +0530

    UI added

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+UPDATE!: 
+1. This game now have a UI.
+2. The script of the console version has been moved to 'original.js'
+
+===========================================
+
 Console version of a simple game "Rock Paper Scissors" created by Nishad PN under The Odin Project.
-To play this game start with "game()" function.
+Console ver : To play this game start with "game()" function.
--- main.js
+++ main.js
@@ -1,71 +1,97 @@
 //@ts-check
+
+//Computer Choices in array
 const choices = ["rock", "paper", "scissors"];
+
+
+// Game Score
 let playerScore=0;
 let computerScore=0;
 let draw=0;
 let games=0;
+
+
 function computerPlay() {
   let randomString = Math.floor(Math.random() * choices.length);
   let randomChoice = choices[randomString];
   let output = randomChoice.charAt(0).toUpperCase() + randomChoice.slice(1);
   return output;
 }
+
+
 function playerSelection() {
-  let input = prompt("Enter your Choice.");
-  let lower = input.toLowerCase();
-  if (lower == "rock" || lower == "paper" || lower == "scissors") {
-    let output = lower.charAt(0).toUpperCase() + lower.slice(1);
+  let input = document.getElementById('button').value;
+  //let lower = input.toLowerCase();
+  //console.log(lower);
+  if (input == "rock" || input == "paper" || input == "scissors") {
+    let output = input.charAt(0).toUpperCase() + input.slice(1);
     return output;
   } else {
-    console.log(
-      "Please enter a valid item from 'Rock', 'Paper' or 'Scissors'."
-    );
+    console.log("Please select a valid item from 'Rock', 'Paper' or 'Scissors'.");
   }
 }
+
+
 function playRound() {
   let player = playerSelection();
   let computer = computerPlay();
+  const container = document.getElementById('message');
   if (player === computer) {
     draw++;
-    return console.log(
-      "Draw! The computer and you both selected " + player + "."
-    );
+    let para=document.createElement('p');
+    let text=para.textContent="Draw! The computer and you both selected " + player + ".";
+    container.appendChild(para);
   } else if (
     (player === "Rock" && computer === "Scissors") ||
     (player === "Paper" && computer === "Rock") ||
     (player === "Scissors" && computer === "Paper")
   ) {
     playerScore++;
-    return console.log(
-      "You win this round! " + player + " beats " + computer + "."
-    );
+    let para=document.createElement('p');
+    let text=para.textContent="You win this round! " + player + " beats " + computer + ".";
+    container.appendChild(para);
   } else {
     computerScore++;
-    return console.log(
-      "You lose this round.." + computer + " beats " + player + "."
-    );
+    let para=document.createElement('p');
+    let text=para.textContent="You lose this round.." + computer + " beats " + player + ".";
+    container.appendChild(para);
   }
+  game();
 }
+
+
 function scoreCalculator(){
   let humanScore=playerScore;
   let botScore=computerScore;
+  const container = document.getElementById('message');
   if(humanScore==botScore){
-    return console.log("This game is tie.");
+    let para=document.createElement('p');
+    let text=para.textContent="This game is draw.";
+    container.appendChild(para);
   }
   else if(humanScore<botScore){
-    return console.log("You lose this game! :'(");
+    let para=document.createElement('p');
+    let text=para.textContent="You lose this game! :'(";
+    container.appendChild(para);
   }
   else if(humanScore>botScore){
-    return console.log("Yay!, you won this game! :)");
+    let para=document.createElement('p');
+    let text=para.textContent="Yay!, you won this game! :)";
+    container.appendChild(para);
   }
 }
+
+
 function game() {
-  if(games==5){
+  const container = document.getElementById('message');
+if(games==5){
     scoreCalculator();
-    return console.log("You: "+ playerScore +" Computer: "+ computerScore +" Draw: "+ draw);
+    let para=document.createElement('p');
+    let text=para.textContent="You: "+ playerScore +" Computer: "+ computerScore +" Draw: "+ draw;
+    container.appendChild(para);
   }
   else{
     games++;
-    playRound();
+  //  playRound();
   }
 }
